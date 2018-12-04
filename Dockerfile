ef7b100d8480f2afd7e11766d897cc05637acc43
Adam Knight <adam@rakenapp.com>
2018-12-04 22:04:07 +1300

    Saving before removing SASS

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,20 @@
-FROM node:8-alpine
-COPY src .
+FROM node:8
+
+WORKDIR /app
+ADD package.json .
+ADD package-lock.json .
+ADD src src
+ADD html html
+
+RUN npm install
+RUN npm run-script sass
 
 EXPOSE 3000
 
-ARG upstream
-ARG label
+ARG upstream=https://jsonplaceholder.typicode.com/todos/1
+ARG label=unknown
 
 ENV SNR_UPSTREAM=${upstream}
 ENV SNR_LABEL=${label}
 
-RUN node index.js
+CMD npm start
--- src/index.js
+++ src/index.js
@@ -1,44 +1,121 @@
-var http = require("http")
-var request = require("request")
+const express = require("express")
+const { Tracer } = require('zipkin');
+const { BatchRecorder } = require('zipkin');
+const { HttpLogger } = require('zipkin-transport-http');
+const CLSContext = require('zipkin-context-cls');
+const fetch = require('node-fetch');
+const wrapFetch = require('zipkin-instrumentation-fetch');
+const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
+
+const app = express()
+const ctxImpl = new CLSContext();
+
+const recorder = new BatchRecorder({
+  logger: new HttpLogger({
+    endpoint: `http://zipkin-collector.dev.rakenapp.com/api/v1/spans`
+  })
+});
+
+const tracer = new Tracer({ ctxImpl, recorder });
 
 const port = process.env.SNR_PORT || 3000;
 const upstream = process.env.SNR_UPSTREAM || "https://jsonplaceholder.typicode.com/todos/1";
 const label = process.env.SNR_LABEL || "unknown";
+const debug = process.env.SNR_DEBUG || false;
 
-const handler = (req, res) => {
+var logDebug = function(message) {
+  if(logDebug) {
+    console.log(`${label}: ${message}`);
+  }
+}
+
+var staticOptions = {
+  dotfiles: 'ignore',
+  etag: true,
+  extensions: ['html'],
+  //maxAge: '1d',
+  redirect: false
+}
+
+app.use(express.static('html', staticOptions));
+
+app.use(zipkinMiddleware({
+  tracer,
+  serviceName: label
+}));
+
+const zipkinFetch = wrapFetch(fetch, {
+  tracer,
+  serviceName: upstream
+});
+
+app.get('/api', (req, res) => {
   // Handle the HTTP Request
   // We want to make a request to the upstream
 
   //console.log(req.headers);
   //console.log(req.headers["x-failpercent"]);
 
+  var options = {};
+
+  logDebug("Checking the Failure header")
   if(req.headers["x-failpercent"]) {
+    options.headers = {};
+    options.headers["x-failpercent"] = req.headers["x-failpercent"]
+
     var failPercent = parseInt(req.headers["x-failpercent"]);
     var chance = Math.random() * 100;
     //console.log(`chance is ${chance}`);
+    logDebug(`Failure rate is ${failPercent} and I rolled a ${chance}`)
     if(chance < failPercent) {
+      logDebug(`${label} had a random failure`)
       res.statusCode = 500;
-      res.statusMessage = `${label} failed`;
+      res.statusMessage = `${label} had a random failure`;
       res.end();
       return;
     }
   }
 
-  request(upstream, function(error, response, body) {
-    //console.log("Error: ", error);
-    //console.log("Response: ", response && response.statusCode);
-    //console.log("Body: ", body)
+  logDebug("Making the fetch")
+  zipkinFetch(upstream, options)
+    .then(upstreamResponse => {
+      logDebug("Processing the response")
+      var annotatedResponse = {};
+      annotatedResponse.label = label;
+      //console.log("upstream.upstream set to: ", body.upstream);
+      annotatedResponse.upstream = {};
+      annotatedResponse.upstream.server = upstream;
+      annotatedResponse.upstream.status = upstreamResponse.status;
+      annotatedResponse.upstream.statusText = upstreamResponse.statusText;
 
-    var annotatedResponse = {};
-    annotatedResponse.label = label;
-    annotatedResponse.upstream = {};
-    annotatedResponse.upstream.server = upstream;
-    annotatedResponse.upstream.responseCode = response.statusCode;
-    annotatedResponse.upstream.error = error;
-    annotatedResponse.upstream.body = body == null ? "" : JSON.parse(body);
-    res.end(JSON.stringify(annotatedResponse));
-  });
-}
+      upstreamResponse.json().then(json =>{
+        logDebug("Reading the body JSON")
+        //console.log("setting label");
+        annotatedResponse.upstream.label = json.label;
+        //console.log("upstream.label set to: ", body.upstream);
+        annotatedResponse.upstream.upstream = json.upstream;
 
-const server = http.createServer(handler);
-server.listen(port);
+        annotatedResponse.content = json.content ? json.content : json;
+        //console.log("content set to: ", annotatedResponse.content);
+
+        res.end(JSON.stringify(annotatedResponse));
+        logDebug("Writing my response")
+      }).catch(err => {
+        annotatedResponse.upstream.error = err;
+        res.end(JSON.stringify(annotatedResponse));
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.statusCode = 500;
+      res.statusMessage = `${label} failed`;
+      res.content = {};
+      res.end(err);
+    });
+});
+
+
+//const server = http.createServer(handler);
+//server.listen(port);
+
+const server = app.listen(port);
